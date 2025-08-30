335778d9258780f3b6b04b571ae0a4fe5dba52a8
ROMEO <empereurayakawa@gmail.com>
2025-08-31 00:55:23 +0200

    Update autosetname.js

--- Script/events/autosetname.js
+++ Script/events/autosetname.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "autosetname",
 	eventType: ["log:subscribe"],
 	version: "1.0.3",
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "『🎧এꔰꜛ.𝗥𝗢𝗠𝗘𝗢.ꜛꗄꔰ🪶🧘🏻‍♂️』",
 	description: "Automatically set new member nicknames"
 };
 
