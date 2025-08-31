ab856ead1968c96f6cc851dc521aaa113bb9406d
ROMEO <empereurayakawa@gmail.com>
2025-08-31 03:51:40 +0200

    Update 0out.js

--- Script/commands/0out.js
+++ Script/commands/0out.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "out",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "『🎧এꔰꜛ.𝗥𝗢𝗠𝗘𝗢𝗕𝗢𝗧.ꜛꗄꔰ🪶🧘🏻‍♂️』",
     description: "",
     commandCategory: "Admin",
     usages: "out [id]",
