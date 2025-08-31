df4802282178c4cff07adb7327f7b079df42e26c
ROMEO <empereurayakawa@gmail.com>
2025-08-31 03:07:22 +0200

    Update antijoin.js

--- Script/events/antijoin.js
+++ Script/events/antijoin.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antijoin",
  eventType: ["log:subscribe"],
  version: "1.0.0",
- credits: "𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁",
+ credits: "『🎧এꔰꜛ.𝗥𝗢𝗠𝗘𝗢𝗕𝗢𝗧.ꜛꗄꔰ🪶🧘🏻‍♂️』",
  description: "Welcome new members to the group"
 };
 
@@ -20,6 +20,6 @@ module.exports.run = async function ({ event, api, Threads, Users }) {
                               global.data.threadData.set(event.threadID, data);
                     })
 			}
- 	return api.sendMessage(`» Your team now has Anti Join mode turned on, please turn it off before adding a new 👻 member`, event.threadID);
+ 	return api.sendMessage(`» 🎧Your team now has Anti Join mode turned on, please turn it off before adding a new member🪶🧘🏻‍♂️`, event.threadID);
  }
 }
